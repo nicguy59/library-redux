53eacac02de00829765f2a4875a4f830a4820893
nicguy59 <mowen@northislandcollege.ca>
2015-02-19 00:17:27 +0000

    Just Image Fetcher to add!

--- app/services/image_fetcher.rb
+++ app/services/image_fetcher.rb
@@ -6,36 +6,38 @@
 #
 # The `fetch` method returns an image URL, or an empty string if no image was found.
 class ImageFetcher
-def fetch(term)
-url = query_url(term)
-Rails.logger.debug "Querying #{url}"
- 
-response = Net::HTTP.get_response(URI.parse(url))
-result_from_response(response)
-end
+
+  def fetch(term)
+    url = query_url(term)
+    Rails.logger.debug "Querying #{url}"
+     
+    response = Net::HTTP.get_response(URI.parse(url))
+    result_from_response(response)
+  end
  
 private
  
-def query_url(term)
-params = default_params.merge(q: term)
-'http://ajax.googleapis.com/ajax/services/search/images?' + params.to_param
-end
+  def query_url(term)
+    params = default_params.merge(q: term)
+    'http://ajax.googleapis.com/ajax/services/search/images?' + params.to_param
+  end
  
-def result_from_response(response)
-response = JSON.parse(response.body)
-response['responseData']['results'].first['url']
-rescue
-# no result (or unknown response format), so return an empty string
-''
-end
+  def result_from_response(response)
+    response = JSON.parse(response.body)
+    response['responseData']['results'].first['url']
+    rescue
+    # no result (or unknown response format), so return an empty string
+    ''
+  end
  
-def default_params
-{
-rsz: '8',
-imgsz: 'medium',
-imgtype: 'photo',
-safe: 'active',
-v: '1.0'
-}
-end
+  def default_params
+  {
+  rsz: '8',
+  imgsz: 'medium',
+  imgtype: 'photo',
+  safe: 'active',
+  v: '1.0'
+  }
+  end
+
 end 
